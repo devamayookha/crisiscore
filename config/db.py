4f5f3cbbad6d588fee6d3be9c4f618e4f0f7b31c
B.R Devamayookha <br.devamayookha@gmail.com>
2026-04-18 22:54:51 +0530

    Lightweight mongodb connection

--- config/db.py
+++ config/db.py
@@ -1,18 +1,21 @@
-from pymongo.mongo_client import MongoClient
-from pymongo.server_api import ServerApi
+from pymongo import MongoClient
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 
+_db = None
+
 def get_db():
+    global _db
+    if _db is not None:
+        return _db
     try:
         uri = os.getenv("MONGO_URI")
-        client = MongoClient(uri, server_api=ServerApi('1'))
-        client.admin.command('ping')
-        db = client["crisiscore"]
+        client = MongoClient(uri)
+        _db = client["crisiscore"]
         print("MongoDB connected!")
-        return db
+        return _db
     except Exception as e:
         print(f"MongoDB connection failed: {e}")
         return None
