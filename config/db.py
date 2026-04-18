ed9c168e0ae564c2a22c5d0a6e27654154811a27
B.R Devamayookha <br.devamayookha@gmail.com>
2026-04-18 22:43:50 +0530

    fix mongodb with server api

--- config/db.py
+++ config/db.py
@@ -1,4 +1,5 @@
-from pymongo import MongoClient
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
 from dotenv import load_dotenv
 import os
 
@@ -7,17 +8,9 @@ load_dotenv()
 def get_db():
     try:
         uri = os.getenv("MONGO_URI")
-        client = MongoClient(
-            uri,
-            serverSelectionTimeoutMS=10000,
-            connectTimeoutMS=10000,
-            socketTimeoutMS=10000,
-            tls=True,
-            tlsAllowInvalidCertificates=True,
-            retryWrites=True
-        )
+        client = MongoClient(uri, server_api=ServerApi('1'))
+        client.admin.command('ping')
         db = client["crisiscore"]
-        db.command("ping")
         print("MongoDB connected!")
         return db
     except Exception as e:
