709289978176f3c3f796e5cd55949ee791b71231
B.R Devamayookha <br.devamayookha@gmail.com>
2026-04-18 21:43:19 +0530

    fix mongodb ssl

--- config/db.py
+++ config/db.py
@@ -7,7 +7,12 @@ load_dotenv()
 def get_db():
     try:
         uri = os.getenv("MONGO_URI")
-        client = MongoClient(uri, serverSelectionTimeoutMS=5000)
+        client = MongoClient(
+            uri,
+            serverSelectionTimeoutMS=5000,
+            tls=True,
+            tlsAllowInvalidCertificates=True
+        )
         client.server_info()
         db = client["crisiscore"]
         print("MongoDB connected!")
