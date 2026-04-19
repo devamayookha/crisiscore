1d8d072a1987851ffa25ef70a6706383af366421
B.R Devamayookha <br.devamayookha@gmail.com>
2026-04-19 10:18:14 +0530

    fix db connection

--- config/db.py
+++ config/db.py
@@ -4,18 +4,13 @@ import os
 
 load_dotenv()
 
-_db = None
-
 def get_db():
-    global _db
-    if _db is not None:
-        return _db
     try:
         uri = os.getenv("MONGO_URI")
         client = MongoClient(uri)
-        _db = client["crisiscore"]
+        db = client["crisiscore"]
         print("MongoDB connected!")
-        return _db
+        return db
     except Exception as e:
         print(f"MongoDB connection failed: {e}")
         return None
